04440b21736886cd0cdccaec40e26ede6591e69c
erikslewis <Adelina198527@gmail.com>
2017-04-24 15:20:47 -0400

    new130

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NODE_ENV === 'production' ?
                       '//enigmatic-mountain-38641.herokuapp.com/api/' :
-                      'https://desolate-earth-24328.herokuapp.com';
+                      'http://desolate-earth-24328.herokuapp.com';
 
 export const WS_URL =  process.env.NODE_ENV === 'production' ?
                        'wss://enigmatic-mountain-38641.herokuapp.com/api/cable' :
