62df21e23a0966bb61e88c2842a60e48c1558a59
erikslewis <Adelina198527@gmail.com>
2017-04-24 16:26:37 -0400

    better3

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11,7 +11,7 @@ export const getAvatarUrl = (avatarUrl) => {
 
 // Helper  function for Post picture image
 export const getImageUrl = (imageUrl) => {
-  return imageUrl ? `${process.env.NODE_ENV !== 'production' ? '://localhost:5000' : ''}${imageUrl}` : noImage;
+  return imageUrl ? `${process.env.NODE_ENV !== 'production' ? '' : ''}${imageUrl}` : noImage;
 }
 
 // Helper function for pluralization
