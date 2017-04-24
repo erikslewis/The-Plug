67c16043405e554a8aa002d709961315ab68c4b8
erikslewis <Adelina198527@gmail.com>
2017-04-24 13:45:57 -0400

    new100

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,4 +4,4 @@ export const API_URL = process.env.NODE_ENV === 'production' ?
 
 export const WS_URL =  process.env.NODE_ENV === 'production' ?
                        'wss://enigmatic-mountain-38641.herokuapp.com/api/cable' :
-                       'ws://localhost:5000/api/cable';
+                       'ws://enigmatic-mountain-38641.herokuapp.com/api/cable';
