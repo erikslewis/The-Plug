73862c21673a09e04f5092f8d20cec0a6f5c2cf9
erikslewis <Adelina198527@gmail.com>
2017-04-24 19:56:49 -0400

    47

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NODE_ENV === 'production' ?
-                      'https://enigmatic-mountain-38641.herokuapp.com/api' :
-                      'https://enigmatic-mountain-38641.herokuapp.com/api';
+                      '//peaceful-headland-77668.herokuapp.com/api' :
+                      '//peaceful-headland-77668.herokuapp.com/api';
 
 export const WS_URL =  process.env.NODE_ENV === 'production' ?
                        '//peaceful-headland-77668.herokuapp.com/api/cable' :
