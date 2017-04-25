b02e27877b71d3ea010b0b52fa4499e241166c75
erikslewis <Adelina198527@gmail.com>
2017-04-25 19:54:33 -0400

    fix6

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.NODE_ENV === 'production' ?
-                      'https://enigmatic-mountain-38641.herokuapp.com/api' :
+                      'https://desolate-earth-24328.herokuapp.com/api' :
                       'https://enigmatic-mountain-38641.herokuapp.com/api';
 
 export const WS_URL =  process.env.NODE_ENV === 'production' ?
